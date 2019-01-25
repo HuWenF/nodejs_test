ef4eb09de772fc2b435eb545fcc53ed73fdf2fe5
HuWenF <qw4749105@yahoo.com>
2019-01-25 15:54:16 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+var http = require('http');
+
+http.createServer(function (request, response) {
+
+    // 发送 HTTP 头部 
+    // HTTP 状态值: 200 : OK
+    // 内容类型: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    // 发送响应数据 "Hello World"
+    //console.log(request.rawHeaders)
+    console.log(request.headers)
+    console.log(request.url)
+    response.end('Hello HWF\n');
+
+}).listen(8888);
+
+// 终端打印如下信息
+console.log('Server running at http://192.168.32.5:8888/');
